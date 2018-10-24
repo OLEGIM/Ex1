7af04781bb72ce34a0a65686884d22c5950058b5
OLEGIM <olegim.89@mail.ru>
2018-10-24 16:02:06 +0300

    REV 3

--- Lesson23-RegEx.py
+++ Lesson23-RegEx.py
@@ -1,8 +1,9 @@
 import re
 
-mytext = "kolya, 22, 33, dasha," \
+mytext = "kolya, 22, 33, dasha, kolya78@" \
          "slava, 27, 24, masha," \
          "vasya, 26, 21, olya," \ 
          "petya, 35, 31, anna"
 
 textlookfor
+
