b9bdf55d68eea9e8f72e93d7a12655dad70d45cd
OLEGIM <olegim.89@mail.ru>
2018-10-26 17:26:11 +0300

    REV 3

--- Lesson26-PyGame2.py
+++ Lesson26-PyGame2.py
@@ -5,18 +5,29 @@ Max_Y = 600
 game_over = False
 
 pygame.init()
-screen = pygame.display.set_mode((Max_X, Max_Y), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((Max_X, Max_Y))
 pygame.display.set_caption("CUBE")
 
 myimage = pygame.image.load("CUBE.png").convert()
-myimage = pygame.transform.scale(myimage, (100 , 100 ))
+myimage = pygame.transform.scale(myimage, (100, 100))
+
+x = 500
+y = 100
 
 while game_over == False:
     for event in pygame.event.get():
         if event == pygame.KEYDOWN:
-            game_over = True
+            if event.key == pygame.K_ESCAPE:
+                game_over = True
+            if event.key == pygame.K_LEFT:
+                x -= 20
+            if event.key == pygame.K_RIGHT:
+                x += 20
+            if event.key == pygame.K_UP:
+                y += 20
+            if event.key == pygame.K_DOWN:
+                y -= 20
 
-    screen.blit(myimage, (100, 100))
+    screen.blit(myimage, (x, y))
     pygame.display.flip()
 
-# print(pygame.image.get_extended())
